cf57787c0f48e5d7f2da47cc1c33cb5cde30152c
Christopher-Law <christopher-law@live.com>
2024-06-12 13:20:11 -0400

    add product category boilerplate

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductCategory extends Model
+{
+    use HasFactory, HasTimestamps, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+}
---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProductCategory;
+use Illuminate\Database\Seeder;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ProductCategory::factory()->create([
+            'name' => 'card',
+            'slug' => 'card',
+        ]);
+    }
+}
