2faf5a7c6323529488ef795a80fa91dbf2d0cec4
Steve McDougall <juststevemcd@gmail.com>
2024-06-14 10:11:28 +0100

    :chore: Updating dev:setup command

--- app/Console/Commands/SetUpDevelopmentEnvironment.php
+++ app/Console/Commands/SetUpDevelopmentEnvironment.php
@@ -1,61 +1,96 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands;
 
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
+use function Laravel\Prompts\{error, info, progress, suggest};
+use Laravel\Sanctum\NewAccessToken;
+use Symfony\Component\Console\Attribute\AsCommand;
 
-class SetUpDevelopmentEnvironment extends Command
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+
+#[AsCommand(name: 'dev:setup', description: 'Set up the development environment.')]
+final class SetUpDevelopmentEnvironment extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'dev:setup';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Set up the development environment';
-
-    /**
-     * Execute the console command.
-     */
-    public function handle(): void
+    public function handle(): int
     {
         if (app()->isProduction()) {
-            $this->error('This command can only be run in a development environment.');
+            error(
+                message: 'This command can only be run in a development environment.'
+            );
 
-            return;
+            return SymfonyCommand::FAILURE;
         }
 
-        $this->info('Setting up development environment...');
+        info(
+            message: 'Setting up development environment...',
+        );
 
-        $this->info('Clearing cache...');
-        Artisan::call('optimize:clear');
+        info(
+            message: 'Clearing cache ...',
+        );
 
-        $this->info('Migrating database...');
-        Artisan::call('migrate:fresh --force');
+        $this->call(
+            command: 'optimize:clear',
+        );
 
-        $this->info('Seeding database...');
-        Artisan::call('db:seed --force');
+        info(
+            message: 'Migrating and seeding database...',
+        );
 
-        $user = User::factory()
-            ->create([
-                'name' => 'Developer Account',
-                'email' => config('development.email'),
+        $this->call(
+            command: 'migrate:fresh',
+            arguments: [
+                '--force' => true,
+                '--seed' => true,
+            ]
+        );
+
+        $name = suggest(
+            label: 'What is your name?',
+            options: ['Taylor', 'Dayle'],
+            placeholder: 'Your name',
+            default: 'Developer Account',
+        );
+
+        $email = suggest(
+            label: 'What is your email?',
+            options: ['test@example.com', 'taylor@laravel.com'],
+            placeholder: 'Your email address',
+            default: config('development.email'),
+        );
+
+        info(
+            message: 'Creating your user account ...',
+        );
+
+        /** @var User $user */
+        $user = User::query()->create(
+            attributes: [
+                'name' => $name,
+                'email' => $email,
                 'password' => Hash::make('password'),
                 'email_verified_at' => now(),
-            ]);
+            ],
+        );
 
-        $token = $user->createToken('development');
+        info(
+            message: 'Creating your API Token ...',
+        );
 
-        $this->info("Your access token is {$token->plainTextToken}");
+        $token = $user->createToken(
+            name: 'development',
+        );
 
+        info(
+            message: "Your access token is {$token->plainTextToken}",
+        );
+
+        return SymfonyCommand::SUCCESS;
     }
 }
