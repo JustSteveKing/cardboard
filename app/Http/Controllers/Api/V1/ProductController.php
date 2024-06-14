14bd97eec4fcbee2b88a79f1a6d977073ff45cee
Christopher-Law <christopher-law@live.com>
2024-06-13 20:06:29 -0400

    Remove controller extension

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Auth\ProductRequest;
 use App\Http\Resources\Api\V1\ProductResource;
 use App\Models\Product;
 
-class ProductController extends Controller
+class ProductController
 {
     public function __invoke(ProductRequest $request)
     {
--- app/Http/Controllers/Api/V1/ProductsController.php
+++ app/Http/Controllers/Api/V1/ProductsController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\ProductResource;
 use App\Models\Product;
 
-class ProductsController extends Controller
+class ProductsController
 {
     public function __invoke()
     {
