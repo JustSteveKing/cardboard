1d908cf05ebdb766337c911405ae3bf9e763ca91
Christopher-Law <christopher-law@live.com>
2024-06-13 23:18:04 -0400

    update template to consider rate limiting and pagination amounts for calculation

--- resources/views/components/documentation/sections/misc.blade.php
+++ resources/views/components/documentation/sections/misc.blade.php
@@ -9,7 +9,7 @@
     <h3 class="text-lg font-semibold">Rate Limiting</h3>
     <p>
         The API allows for 60 requests per minute from a given user/IP address.
-        The <code>/api/v1/products</code> endpoint returns 100 items per request.
-        This means you could update ~150,000 product prices in ~30 minutes.
+        The <code>/api/v1/products</code> endpoint returns {{config('app.products_pagination_amount')}} items per request.
+        This means you could update ~{{\App\Models\Product::count()}} product prices in ~{{ round(\App\Models\Product::count()/(config('app.products_pagination_amount')*config('app.api_rate_limit')))}} minutes.
     </p>
 </div>
